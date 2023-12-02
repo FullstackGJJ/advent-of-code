ec322b5fccd263694675771aad18e2365580b1fe
Hank Lin <fullstackgjj@gmail.com>
2023-12-02 14:39:00 -0500

    saving work

--- 2023/day-1-trebuchet-helpers.scm
+++ 2023/day-1-trebuchet-helpers.scm
@@ -1,9 +1,11 @@
 ;; Files labeled helpers are intended to only be used by targeted audience
 ;;   it's named after and unit test runners
-(module day-1-trebuchet-helpers
-
-  (find-first-char-digit-or-word-digit
-   find-last-char-digit-or-word-digit)
+;;(module day-1-trebuchet-helpers
+;;
+;;  (find-first-char-digit-or-word-digit
+;;   find-last-char-digit-or-word-digit
+;;   substring-indices
+;;)
 
   (import
     scheme
@@ -14,7 +16,17 @@
 
   (define digit-word-to-digit-char-map
     (alist->hash-table
-      '(("one" . #\1)
+      '(("1" . #\1)
+        ("2" . #\2)
+        ("3" . #\3)
+        ("4" . #\4)
+        ("5" . #\5)
+        ("6" . #\6)
+        ("7" . #\7)
+        ("8" . #\8)
+        ("9" . #\9)
+        ;; Commenting below will give result for aoc 2023 day 1 part 1
+        ("one" . #\1)
         ("two" . #\2)
         ("three" . #\3)
         ("four" . #\4)
@@ -22,13 +34,58 @@
         ("six" . #\6)
         ("seven" . #\7)
         ("eight" . #\8)
-        ("nine" . #\9))))
-
-  (define word-digits
-    '("one" "two" "three" "four" "five" "six" "seven" "eight" "nine"))
+        ("nine" . #\9)
+        ;; Commenting above will give result for aoc 2023 day 1 part 1
+        )))
 
   (define (digit-word->digit-char word) 
     (hash-table-ref digit-word-to-digit-char-map word))
+
+  (define (substring-indices str1 str2 #!optional (cursor 0))
+    (let [(substring-index (string-contains str1 str2))
+          (substring-length (string-length str2))
+          ]
+      (if (eq? #f substring-index)
+        '()
+        (cons
+          (+ substring-index cursor)
+          (substring-indices
+            (string-drop str1 (+ substring-length substring-index))
+            str2
+            (+ cursor (+ substring-length substring-index)))))))
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ; (define substring-index (string-contains "six92onesix" "six"))
+  ; (0 8)
+  ; (substring-indices "six92onesix" "six")
+  ; (3)
+  ; (substring-indices "8six1ninezjsix" "six")
+  ; (1 11)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+  (define ()
+    ())
+
+  (define word-digits
+    '("1"
+      "2"
+      "3"
+      "4"
+      "5"
+      "6"
+      "7"
+      "8"
+      "9"
+      "one"
+      "two"
+      "three"
+      "four"
+      "five"
+      "six"
+      "seven"
+      "eight"
+      "nine"))
 
   (define (find-first-char-numeric-from-head line)
     (let [(chars (string->list line))]
@@ -40,6 +97,7 @@
 
   (define (valid-position position)
     (not (eq? #f (cadr position))))
+
 
   (define (find-first-word-digit-from-head line)
     (fold
@@ -115,7 +173,7 @@
                  valid-position
                  (list find-last-char-numeric-result find-last-word-digit-result))))))
 
-) ;; End of module
+;;) ;; End of module
 
 
 ;(define x (list ("one" #f)  ("two" 0)  ("three" #f)  ("four" #f)  ("five" #f)  ("six" #f)  ("seven" #f)  ("eight" #f)  ("nine" 4))) 
--- 2023/day-1-trebuchet-helpers.tests.scm
+++ 2023/day-1-trebuchet-helpers.tests.scm
@@ -1,3 +1,9 @@
+;; Egg imports
 (import scheme srfi-64)
+
+;; Project imports
+(import day-1-trebuchet-helpers)
+
+;; Testing section
 (test-begin "day-1-trebuchet-helpers")
 (test-end "day-1-trebuchet-helpers")
